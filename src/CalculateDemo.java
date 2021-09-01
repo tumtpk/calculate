caf1ee73c8c8ce480b89faf7c8b9007b88c2474f
tumtpk <pk.prateep@gmail.com>
2021-09-01 15:15:23 +0700

    add fn main

--- src/CalculateDemo.java
+++ src/CalculateDemo.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class CalculateDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input Score ");
+		int score = in.nextInt();
+		CalculateGrade c = new CalculateGrade(score);
+		System.out.println("grade "+c.calcuate());
 	}
 
 }
