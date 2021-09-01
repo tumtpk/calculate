3c4631b5f42dfdd130fdf42643f6c5d5d47418ff
tumtpk <pk.prateep@gmail.com>
2021-09-01 15:09:24 +0700

    edit structurer Calculate class

--- src/CalculateGrade.java
+++ src/CalculateGrade.java
@@ -1,4 +1,21 @@
 
 public class CalculateGrade {
+	private int score;
 
+	public CalculateGrade(int score) {
+		
+	}
+
+	public int getScore() {
+		return 0;
+	}
+
+	public void setScore(int score) {
+		
+	}
+	
+	public char calcuate() {
+		return 'A';
+	}
+	
 }
