f3f567bcc78f394003e95b69bdc0127e3839877d
tumtpk <pk.prateep@gmail.com>
2021-09-01 15:18:22 +0700

    edit Calculate Class

--- src/CalculateGrade.java
+++ src/CalculateGrade.java
@@ -3,19 +3,33 @@ public class CalculateGrade {
 	private int score;
 
 	public CalculateGrade(int score) {
-		
+		this.score = score;
 	}
 
 	public int getScore() {
-		return 0;
+		return score;
 	}
 
 	public void setScore(int score) {
-		
+		this.score = score;
 	}
 	
 	public char calcuate() {
-		return 'A';
+		char grade;
+		
+		if(score >= 80) {
+			grade = 'A';
+		}else if(score >= 70) {
+			grade = 'B';
+		}else if(score >= 60) {
+			grade = 'C';
+		}else if(score >= 50) {
+			grade = 'D';
+		}else {
+			grade = 'F';
+		}
+		
+		return grade;
 	}
 	
 }
